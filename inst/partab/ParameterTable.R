78cbbfdd93de9d71a1853ed116c2c0220fd086b8
Tim Bergsma <bergsmat@gmail.com>
2023-05-10 06:40:07 +0000

    version 1.0.1

--- inst/partab/ParameterTable.R
+++
@@ -1,5 +0,0 @@
-# library(nonmemica)
-# dir.NONMEM <- "C:/Desktop/Example" # This is the general directory where the folders of the runs are located.
-options(project = '.')
-undebug(nonmemica:::partab.character)
-partab("logreg", relative=F)
